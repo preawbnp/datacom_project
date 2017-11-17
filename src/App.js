38b24d5ed187754763537dc0e9d7ea09d8cf6ac9
preawbnp <preawbnp@preawbnps-MacBook-Pro.local>
2017-11-18 01:10:32 +0700

    add mockup google-map layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ip_number: '',
-      title: "Click here"      
+      ip_number: ''
     }
   }
-
-  changeTitle = () => {
-    this.setState({ title: "New title" });
- };
-
   
+  static defaultProps = {
+    center: {lat: 13.7563, lng: 100.5018},
+    zoom: 13
+  };
+
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          <span>Your IP Address:</span>          
-          <span id="IP"></span>
-          <br/>
-          <button onClick={this.changeTitle}>{this.state.title}</button>
-        </p>
-        
-      </div>
+        <div className="row">
+          <div className="col-sm-4">
+            <GoogleMapReact
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >
+            <AnyReactComponent
+              lat={13.7563}
+              lng={100.5018}
+            />
+            </GoogleMapReact>
+          </div>
+          
+          <div className="col-sm-8">
+
+          </div>
+          </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
 import './stylesheets/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
